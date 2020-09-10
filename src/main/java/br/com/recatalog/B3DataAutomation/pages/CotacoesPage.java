e92c7069e2e52adf087b4556b6d438085cb9a8f0
José Fernando Pereira <jose.ze.fernando@gmail.com>
2020-09-10 05:56:25 -0300

    resolvido erro de travar com  executor

--- src/main/java/br/com/recatalog/B3DataAutomation/pages/CotacoesPage.java
+++ src/main/java/br/com/recatalog/B3DataAutomation/pages/CotacoesPage.java
@@ -1,14 +1,21 @@
 package br.com.recatalog.B3DataAutomation.pages;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import br.com.recatalog.B3DataAutomation.base.BasePage;
 
@@ -28,26 +35,47 @@ public class CotacoesPage extends BasePage {
 		driver.switchTo().defaultContent(); //new
 		driver.switchTo().frame("bvmf_iframe");
 		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
 		WebElement dataEl = driver.findElement(By.xpath("//div[@class='DayPickerInput']/input"));
 		dataEl.sendKeys(Keys.CONTROL + "a");
 		dataEl.sendKeys(Keys.DELETE);
 		dataEl.sendKeys(data);
 		dataEl.sendKeys(Keys.ENTER);
 		
-		WebElement stockCodeEl = driver.findElement(By.cssSelector("input[type='text']"));
+		Instant previous = Instant.now();
+
+		WebElement stockCodeEl = null;
+		WebElement pesquisarBtn = null;
+		WebElement ativoPesquisadoLink = null;
+		
 		for(String code : stockCodes) {
+	        JavascriptExecutor js = (JavascriptExecutor) driver;
+	        
+			stockCodeEl = driver.findElement(By.cssSelector("input[type='text']"));
+			previous = Instant.now();
 			stockCodeEl.sendKeys(Keys.CONTROL + "a");
 			stockCodeEl.sendKeys(Keys.DELETE);
 			stockCodeEl.sendKeys(code);
 			stockCodeEl.sendKeys(Keys.ENTER);
-			WebElement pesquisarBtn = driver.findElement(By.cssSelector("input[type='submit']"));
-			pesquisarBtn.click();
+
+			WebDriverWait wait0 = new WebDriverWait(driver, 10);
+			
+//			wait0.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='modal-body']")));
+//			wait0.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//a[@data-dropdown='Section-2']")));
+//			wait0.until(ExpectedConditions.visibilityOf(pesquisarBtn));
+			wait0.ignoring(StaleElementReferenceException.class)
+			.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='submit' and @class='button expand']")));
+				pesquisarBtn = driver.findElement(By.xpath("//input[@type='submit' and @class='button expand']"));
+
+				js.executeScript("arguments[0].click()", pesquisarBtn);
 			try {
-				WebElement ativoPesquisadoLink = driver.findElement(By.linkText("Ativo pesquisado"));
+				String var = "//a[contains(@href,'" + code + "')]";
+				ativoPesquisadoLink = driver.findElement(By.xpath(var));
+
+				WebDriverWait wait = new WebDriverWait(driver, 10);
+				wait.until(ExpectedConditions.elementToBeClickable(By.xpath(var)));
 				ativoPesquisadoLink.click();
-			} catch(StaleElementReferenceException e) {
-				System.out.println("Sem processamento para a ação: " + code  );
-				continue;
 			} catch(NoSuchElementException e) {
 				System.out.println("Sem processamento para a ação: " + code  );
 				continue;
@@ -55,25 +83,74 @@ public class CotacoesPage extends BasePage {
 				System.err.println("TimeoutException ");
 				e.printStackTrace();
 				break;
+			}catch (ElementClickInterceptedException e) {
+				e.printStackTrace();
+				continue;
 			}
+			System.out.println("Code : " + code + " " + ChronoUnit.MILLIS.between(previous,Instant.now()));
 		}
 		
-		try {
-			Thread.sleep(30000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		driver.close();
-		driver.quit();
+//		driver.close();
+//		driver.quit();
 	}
 
 	
 	public static void main (String[] args) {
 		CotacoesPage cot = new CotacoesPage();
 		cot.initialization("CHROME", "http://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/cotacoes/cotacoes");
-		System.out.println(cot.validatePage());
-		List<String> codes = Arrays.asList("PETR4","VVAR3");
-		cot.downloadNegociosByStockUntilNow(codes, "04/09/2020");
+		List<String> codes = Arrays.asList(
+				"ADHM3",
+				"TIET11",
+				"TIET3",
+				"TIET4",
+				"AFLT3",
+				"RPAD3",
+				"ALSO3",
+				"ALPK3",
+				"ALPA3",
+				"APER3",
+				"ALUP11",
+				"ALUP3",
+				"ALUP4",
+				"ABEV3",
+				"AMBP3",
+				"CBEE3",
+				"ANIM3",
+				"ARZZ3",
+				"CRFB3",
+				"ATOM3",
+				"AZEV3",
+				"AZUL4",
+				"BTOW3",
+				"B3SA3",
+				"BAHI3",
+				"BMGB4",
+				"BIDI11",
+				"BIDI3",
+				"BIDI4",
+				"BEES3",
+				"BEES4",
+				"BDLL3",
+				"BDLL4",
+				"BTTL3",
+				"BALM3",
+				"BALM4",
+				"BBSE3",
+				"BBML3",
+				"ABCB4",
+				"BRIV3",
+				"BRIV4",
+				"BAZA3",
+				"BBDC3",
+				"BBDC4",
+				"BBAS11",
+				"BBAS3",
+				"BPAC11",
+				"BPAC3",
+				"BGIP3",
+				"BGIP4",
+				"BPAR3"
+				);
+		cot.downloadNegociosByStockUntilNow(codes, "03/09/2020");
 	}
 }
--- src/main/java/br/com/recatalog/B3DataAutomation/util/Util.java
+++ src/main/java/br/com/recatalog/B3DataAutomation/util/Util.java
@@ -1,5 +1,9 @@
 package br.com.recatalog.B3DataAutomation.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+
 import org.openqa.selenium.WebDriver;
 
 public class Util {
@@ -7,4 +11,37 @@ public class Util {
 	public static WebDriver switchTo(WebDriver driver, String iFrameId) {
 		return  driver.switchTo().frame(iFrameId); 
 	}
+	
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+        
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+        
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+        
+        return destFile;
+    }
+    
+//    public static void main(String[] args) throws IOException {
+//        String fileZip = "src/main/resources/unzipTest/compressed.zip";
+//        File destDir = new File("src/main/resources/unzipTest");
+//        byte[] buffer = new byte[1024];
+//        ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+//        ZipEntry zipEntry = zis.getNextEntry();
+//        while (zipEntry != null) {
+//            File newFile = newFile(destDir, zipEntry);
+//            FileOutputStream fos = new FileOutputStream(newFile);
+//            int len;
+//            while ((len = zis.read(buffer)) > 0) {
+//                fos.write(buffer, 0, len);
+//            }
+//            fos.close();
+//            zipEntry = zis.getNextEntry();
+//        }
+//        zis.closeEntry();
+//        zis.close();
+//    }    
 }
